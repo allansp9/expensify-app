af2016f04163665b5aa1b8ea0518cee84ef36244
Allan <allansp9@gmail.com>
2018-01-29 12:07:18 -0200

    ES6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //   name: "Allan",
 //   age: 27,
@@ -17,14 +21,35 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    // name: "Penguin"
-  }
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     // name: "Penguin"
+//   }
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
+// const { name: publisherName = "Self-Published" } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+// Array destructuring
+//
+
+// const address = [
+//   "1288 S Juniper Street",
+//   "Philadelphia",
+//   "Pennsylvania",
+//   "19147"
+// ];
+
+// const [, city, state = "New York"] = address;
+
+// console.log(`You are in ${city} ${state}.`);
+
+const item = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
