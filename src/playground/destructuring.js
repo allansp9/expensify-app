153c4f1e9d9dc52aa5a1a0a323f3c0d8b77b7c35
Allan <allansp9@gmail.com>
2018-01-29 11:45:35 -0200

    ES6 Object Destructuring

---
+++ src/playground/destructuring.js
@@ -0,0 +1,30 @@
+// const person = {
+//   name: "Allan",
+//   age: 27,
+//   location: {
+//     city: "Santos",
+//     temp: 29
+//   }
+// };
+
+// const { name = "Anonymous", age } = person;
+
+// console.log(`${name} is ${age}.`);
+
+// const { city, temp: temperature } = person.location;
+
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+  title: "Ego is the Enemy",
+  author: "Ryan Holiday",
+  publisher: {
+    // name: "Penguin"
+  }
+};
+
+const { name: publisherName = "Self-Published" } = book.publisher;
+
+console.log(publisherName);
