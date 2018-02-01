d8a2129aec0cc4fd353ff338fe80c6cb862957b8
Allan <allansp9@gmail.com>
2018-02-01 20:48:14 -0200

    testing ExpenseListFilters

--- src/tests/components/ExpenseDashBoard.test.js
+++ src/tests/components/ExpenseDashBoard.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import "react-dates/initialize";
 import ExpenseDashBoard from "../../components/ExpenseDashBoard";
 
 test("should render ExpenseDashBoard correctly", () => {
