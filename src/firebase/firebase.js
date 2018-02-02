f50d494055692e6a2df1fae294338f2e41649118
Allan <allansp9@gmail.com>
2018-02-02 20:56:09 -0200

    writing to the database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,9 +10,22 @@ const config = {
 };
 firebase.initializeApp(config);
 
-firebase
-  .database()
-  .ref()
-  .set({
-    name: "Allan Pereira"
-  });
+const database = firebase.database();
+
+database.ref().set({
+  name: "Allan Pereira",
+  age: 26,
+  isSingle: true,
+  location: {
+    city: "Santos",
+    country: "Brazil"
+  }
+});
+
+// database.ref().set("This is my data");
+
+database.ref("age").set(27);
+database.ref("attributes").set({
+  height: 185,
+  weight: 78
+});
