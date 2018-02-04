364d04d49fa902ef63cfacec230e72cc129ea51b
Allan <allansp9@gmail.com>
2018-02-04 13:43:57 -0200

    Logging out

--- src/tests/components/Login.test.js
+++ src/tests/components/Login.test.js
@@ -6,3 +6,10 @@ test("should render Login Page", () => {
   const wrapper = shallow(<Login />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test("should call startLogin on button click", () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<Login startLogin={startLogin} />);
+  wrapper.find("button").simulate("click");
+  expect(startLogin).toHaveBeenCalled();
+});
