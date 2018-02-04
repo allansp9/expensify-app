d309f6d8638b3835b5e971dd1f47e5afda287472
Allan <allansp9@gmail.com>
2018-02-04 14:03:59 -0200

    redirecting login or logout

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
 import ExpenseDashBoard from "../components/ExpenseDashBoard";
 import AddExpense from "../components/AddExpense";
 import EditExpense from "../components/EditExpense";
@@ -8,8 +9,10 @@ import NotFound from "../components/NotFound";
 import Header from "../components/Header";
 import Login from "../components/Login";
 
+export const history = createHistory();
+
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
       <Header />
       <Switch>
@@ -21,7 +24,7 @@ const AppRouter = () => (
         <Route component={NotFound} />
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
